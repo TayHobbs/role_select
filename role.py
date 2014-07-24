9ab81f6d647e57c07fecc5cd92306c14e69dcd34
Taylor Hobbs <taylor@feemon.com>
2014-07-24 11:33:17 -0500

    Added mocks and removed operator and repleced with for loop and list comprehension

--- role.py
+++ role.py
@@ -1,5 +1,3 @@
-import operator
-
 class Role(object):
 
     def __init__(self, health, magic, choice):
@@ -9,138 +7,155 @@ class Role(object):
 
 class Warrior(Role):
     weapon = 'shield'
-    title = 'The Healing'
+    title = 'The Brave'
 
     def __init__(self):
         super(Warrior, self).__init__(200, 50,  0)
 
 
 class Priest(Role):
-    weapon = 'magic'
-    title = 'The Brave'
+    weapon = 'staff'
+    title = 'The Healing'
 
     def __init__(self):
         super(Priest, self).__init__(100, 200, 0)
 
-# class Rouge(Role):
-#     weapon = 'dagger'
-#     title = 'The Quick'
+class Rouge(Role):
+    weapon = 'daggers'
+    title = 'The Quick'
 
-# class Hunter(Role):
-#     weapon = 'bow'
-#     title = 'The Swift'
+    def __init__(self):
+        super(Rouge, self).__init__(150, 30, 0)
 
-# Warrior = Role(200, 50, 0)
-# Hunter = Role(125, 100, 0)
-# Priest = Role(100, 200, 0)
-# Rouge = Role(150, 30, 0)
+class Hunter(Role):
+    weapon = 'bow'
+    title = 'The Swift'
 
-# print "What class is best for you?"
-
-
-# print "Do you like protecting your friends?"
-# prot = raw_input()
-# if prot == 'yes' or prot == 'Yes':
-#     Warrior.choice += 1
-#     Priest.choice += 2
-# elif prot == 'no' or prot == 'No':
-#     Rouge.choice += 1
-# else:
-#     print "Fine, don't answer."
-#     Hunter.choice += 2
-
-# print "Do you like being a leader?"
-# lead = raw_input()
-# if lead == 'yes' or lead == 'Yes':
-#     Warrior.choice += 1
-# elif lead == 'no' or lead == 'No':
-#     Hunter.choice += 1
-# else:
-#     print "Fine, don't answer."
-
-# print "Do you like being in the backgRound?"
-# back = raw_input()
-# if back == 'yes' or back == 'Yes':
-#     Rouge.choice += 1
-#     Hunter.choice += 2
-# elif back == 'no' or back == 'No':
-#     Priest.choice += 1
-# else:
-#     print "Fine, don't answer."
-
-# print "Do you like dealing damage up close?"
-# close = raw_input()
-# if close == 'yes' or close == 'Yes':
-#     Rouge.choice += 1
-#     Warrior.choice += 1
-# elif close == 'no' or close == 'No':
-#     Hunter.choice += 1
-#     Priest.choice += 1
-# else:
-#     print "Fine, don't answer."
+    def __init__(self):
+        super(Hunter, self).__init__(125, 100, 0)
 
 
-# print "Far aWay?"
-# distance = raw_input()
-# if distance == 'yes' or distance == 'Yes':
-#     Hunter.choice += 2
-#     Priest.choice += 1
-# elif distance == 'no' or distance == 'No':
-#     Rouge.choice += 1
-#     Warrior.choice += 1
-# else:
-#     print "Fine, don't answer."
-#     Warrior.choice += 1
+def role_selection():
 
-# print "Do you like working alone?"
-# alone = raw_input()
-# if alone == 'yes' or alone == 'Yes':
-#     Rouge.choice += 2
-#     Hunter.choice += 1
-#     Warrior.choice -= 1
-# elif alone == 'no' or alone == 'No':
-#     Priest.choice += 2
-# else:
-#     print "Fine, don't answer."
-    
-# print "Do you like wearing heavy, medium or light armor?"
-# armor = raw_input()
-# if armor == 'heavy' or armor == 'Heavy':
-#     Warrior.choice += 3
-# elif armor == 'medium' or armor == 'Medium':
-#     Rouge.choice += 3
-#     Hunter.choice += 3
-# else:
-#     Priest.choice += 3
+    warrior = Warrior()
+    priest = Priest()
+    rouge = Rouge()
+    hunter = Hunter()
 
-# print "Do you like swords, magic or bows?"
-# weapon = raw_input()
-# if weapon == 'swords' or weapon == 'Swords' or weapon == 'sword' or weapon == 'Sword':
-#     Warrior.choice += 2
-#     Rouge.choice += 2
-# elif weapon == 'magic' or weapon == 'Magic':
-#     Priest.choice += 2
-# else:
-#     Hunter.choice += 2
+    roles = [warrior, priest, rouge, hunter]
 
-# # Going to implement later
-# #print "What is your favorite color?"
-# #color = raw_input()
+    print "What class is best for you?"
+    prot = raw_input("Do you like protecting your friends? (y/n) => ")
+    if prot.lower() == 'y':
+        warrior.choice += 1
+        priest.choice += 2
 
-# role = {'Warrior': Warrior.choice, 'Priest': Priest.choice, 'Hunter': Hunter, 'Rouge': Rouge}
+    max_choice = 0
 
-# character =  max(role.iteritems(), key=operator.itemgetter(1))[0]
+    for role in roles:
+        if role.choice > max_choice:
+            max_choice = role.choice
 
-# character = character.capitalize() 
+    return [r for r in roles if r.choice == max_choice][0]
 
-# print "Your class is " + character
+    # elif prot == 'no' or prot == 'No':
+    #     Rouge.choice += 1
+    # else:
+    #     print "Fine, don't answer."
+    #     Hunter.choice += 2
 
-# print "What would you like to name your class?"
-# name = raw_input()
+    # print "Do you like being a leader?"
+    # lead = raw_input()
+    # if lead == 'yes' or lead == 'Yes':
+    #     Warrior.choice += 1
+    # elif lead == 'no' or lead == 'No':
+    #     Hunter.choice += 1
+    # else:
+    #     print "Fine, don't answer."
 
-# print "\n" +  name.capitalize() + " the " + character.title
+    # print "Do you like being in the backgRound?"
+    # back = raw_input()
+    # if back == 'yes' or back == 'Yes':
+    #     Rouge.choice += 1
+    #     Hunter.choice += 2
+    # elif back == 'no' or back == 'No':
+    #     Priest.choice += 1
+    # else:
+    #     print "Fine, don't answer."
 
-# #print Role['Warrior']
-# #print Role['Rouge']
-# #print Role['Priest']
-# #print Role['Hunter']
+    # print "Do you like dealing damage up close?"
+    # close = raw_input()
+    # if close == 'yes' or close == 'Yes':
+    #     Rouge.choice += 1
+    #     Warrior.choice += 1
+    # elif close == 'no' or close == 'No':
+    #     Hunter.choice += 1
+    #     Priest.choice += 1
+    # else:
+    #     print "Fine, don't answer."
+
+
+    # print "Far away?"
+    # distance = raw_input()
+    # if distance == 'yes' or distance == 'Yes':
+    #     Hunter.choice += 2
+    #     Priest.choice += 1
+    # elif distance == 'no' or distance == 'No':
+    #     Rouge.choice += 1
+    #     Warrior.choice += 1
+    # else:
+    #     print "Fine, don't answer."
+    #     Warrior.choice += 1
+
+    # print "Do you like working alone?"
+    # alone = raw_input()
+    # if alone == 'yes' or alone == 'Yes':
+    #     Rouge.choice += 2
+    #     Hunter.choice += 1
+    #     Warrior.choice -= 1
+    # elif alone == 'no' or alone == 'No':
+    #     Priest.choice += 2
+    # else:
+    #     print "Fine, don't answer."
+        
+    # print "Do you like wearing heavy, medium or light armor?"
+    # armor = raw_input()
+    # if armor == 'heavy' or armor == 'Heavy':
+    #     Warrior.choice += 3
+    # elif armor == 'medium' or armor == 'Medium':
+    #     Rouge.choice += 3
+    #     Hunter.choice += 3
+    # else:
+    #     Priest.choice += 3
+
+    # print "Do you like swords, magic or bows?"
+    # weapon = raw_input()
+    # if weapon == 'swords' or weapon == 'Swords' or weapon == 'sword' or weapon == 'Sword':
+    #     Warrior.choice += 2
+    #     Rouge.choice += 2
+    # elif weapon == 'magic' or weapon == 'Magic':
+    #     Priest.choice += 2
+    # else:
+    #     Hunter.choice += 2
+
+    # # Going to implement later
+    # #print "What is your favorite color?"
+    # #color = raw_input()
+
+    # role = {'Warrior': Warrior.choice, 'Priest': Priest.choice, 'Hunter': Hunter, 'Rouge': Rouge}
+
+    # character =  max(role.iteritems(), key=operator.itemgetter(1))[0]
+
+    # character = character.capitalize() 
+
+    # print "Your class is " + character
+
+    # print "What would you like to name your class?"
+    # name = raw_input().capitalize()
+
+    # print "\n" +  name + " the " + character.title
+
+#print Role['Warrior']
+#print Role['Rouge']
+#print Role['Priest']
+#print Role['Hunter']
--- tests.py
+++ tests.py
@@ -1,27 +1,85 @@
 import unittest
-import operator
-from role import Role, Warrior, Priest
+
+import mock
+
+from role import Role, Warrior, Priest, Rouge, Hunter, role_selection
 
 
 class Roletest(unittest.TestCase):
 
 
     def test_that_warrior_has_200_health(self):
-        role = Warrior()
-        self.assertEqual(role.health, 200)
+        self.assertEqual(Warrior().health, 200)
 
     def test_that_warrior_50_magic(self):
-        role = Warrior()
-        self.assertEqual(role.magic, 50)
+        self.assertEqual(Warrior().magic, 50)
 
     def test_that_warrior_has_0_choice(self):
-        role = Warrior()
-        self.assertEqual(role.choice, 0)
+        self.assertEqual(Warrior().choice, 0)
+
+    def test_that_warrior_has_shield(self):
+        self.assertEqual(Warrior().weapon, "shield")
+
+    def test_that_warrior_has_title(self):
+        self.assertEqual(Warrior().title, "The Brave")
     
     def test_that_priest_has_100_health(self):
-        role = Priest()
-        self.assertEqual(role.health, 100)
+        self.assertEqual(Priest().health, 100)
 
     def test_that_priest_has_50_magic(self):
-        role = Priest()
-        self.assertEqual(role.magic, 200)
+        self.assertEqual(Priest().magic, 200)
+
+    def test_that_priest_has_0_choice(self):
+        self.assertEqual(Priest().choice, 0)
+
+    def test_that_priest_has_staff(self):
+        self.assertEqual(Priest().weapon, "staff")
+
+    def test_that_priest_has_title(self):
+        self.assertEqual(Priest().title, "The Healing")
+
+    def test_that_rouge_has_150_health(self):
+        self.assertEqual(Rouge().health, 150)
+
+    def test_that_rouge_has_50_magic(self):
+        self.assertEqual(Rouge().magic, 30)
+
+    def test_that_rouge_has_0_choice(self):
+        self.assertEqual(Rouge().choice, 0)
+
+    def test_that_rouge_has_staff(self):
+        self.assertEqual(Rouge().weapon, "daggers")
+
+    def test_that_rouge_has_title(self):
+        self.assertEqual(Rouge().title, "The Quick")
+
+    def test_that_hunter_has_125_health(self):
+        self.assertEqual(Hunter().health, 125)
+
+    def test_that_hunter_has_100_magic(self):
+        self.assertEqual(Hunter().magic, 100)
+
+    def test_that_hunter_has_0_choice(self):
+        self.assertEqual(Hunter().choice, 0)
+
+    def test_that_hunter_has_staff(self):
+        self.assertEqual(Hunter().weapon, "bow")
+
+    def test_that_hunter_has_title(self):
+        self.assertEqual(Hunter().title, "The Swift")
+
+    @mock.patch("__builtin__.raw_input", mock.Mock(return_value="y"))
+    def test_warrior_role_selection_changes_choice_attribute(self):
+        sut = role_selection()
+        self.assertEqual(sut.choice, 2)
+        self.assertIsInstance(sut, Priest)
+
+    @mock.patch("__builtin__.raw_input", return_value="no")
+    def test_warrior_role_selection_changes_choice_attribute_other_way(self, mock_answer):
+        sut = role_selection()
+        self.assertEqual(sut.choice, 0)
+        self.assertIsInstance(sut, Warrior)
+
+    @mock.patch("__builtin__.raw_input", mock.Mock(return_value="y"))
+    def test_that_character_with_most_choice_is_returned(self):
+        self.assertIsInstance(role_selection(), Priest)
